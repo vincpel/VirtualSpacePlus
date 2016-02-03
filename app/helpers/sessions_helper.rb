20239525068bcd51022af6346692be5215877c82
vincpel <vincpel@gmail.com>
2016-02-02 20:09:01 -0500

    started to include sessions

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -4,4 +4,13 @@ module SessionsHelper
   def log_in(user)
     session[:user_id] = user.id
   end
+
+  def ifLoggin(user)
+    if session[:user_id] == user.id
+
+    else
+      redirect_to login_url
+    end
+  end
+
 end
