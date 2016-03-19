8c0e85a2aca9455fab1a24ffcd8250fc3be33425
vincpel <vincpel@gmail.com>
2016-03-19 09:42:52 -0400

    new book controller

--- app/models/ad.rb
+++ app/models/ad.rb
@@ -1,8 +1,9 @@
 class Ad < ActiveRecord::Base
     belongs_to :user
-
     belongs_to :book
     belongs_to :electronic
     
     self.inheritance_column = nil   # to overide STI 
+    
+
 end
--- app/views/books/show.json.jbuilder
+++
@@ -1,1 +0,0 @@
-json.extract! @book, :id, :title, :description, :isbn, :created_at, :updated_at
