8e767d4b5e49781b2cc5637e3653d06282edf019
vincpel <vincpel@gmail.com>
2016-03-27 19:34:27 -0400

    seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,3 +8,39 @@
 user = CreateAdminService.new.call
 puts 'CREATED ADMIN USER: ' << user.email
 
+user1 = User.find_or_create_by!(email: 'martin@vsplus.com') do |user|
+        user.password = "password"
+        user.password_confirmation = "password"
+end
+puts 'CREATED DUMMY USER ' << user1.email
+
+ad1 = Ad.new()   
+ad1.title = "harry poter et les 7 nains"
+ad1.description = "bleu"
+ad1.price = 5
+ad1.user = user1
+ad1.book = book1 = Book.new()
+
+book1.ad = ad1
+book1.ISBN = 67872364
+
+ad1.save
+book1.save
+
+puts 'CREATED BOOK1 ' << ad1.title
+
+ad2 = Ad.new()   
+ad2.title = "laptop mince"
+ad2.description = "pas cher"
+ad2.price = 500
+ad2.user = user1
+ad2.electronic = electronic1 = Electronic.new()
+
+electronic1.ad = ad2
+electronic1.brand = "Samsung"
+electronic1.model = "Ativ 9 " 
+
+ad2.save
+electronic1.save
+
+puts 'CREATED ELECTRONIC1 ' << ad2.title
